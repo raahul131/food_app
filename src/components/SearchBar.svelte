ce77ef6ce388188e43eae602efac323e1d3cd16a
Rahul R Yadav <rryadav2025@gmail.com>
2023-09-14 16:25:36 +0545

    search baar margin changes

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,14 +1,14 @@
 <script></script>
 
 <main>
-  <div class="flex items-center">
+  <div class="flex items-center ">
     <input
       type="text"
       placeholder="Search recipe"
       class=" w-[220px] mx-8 p-3 border-gray-500 border rounded-2xl"
     />
     <button
-      class="bg-emerald-600 p-2 pr-4 rounded-xl text-lg font-semibold text-white items-center text-center justify-center cursor-pointer"
+      class="bg-emerald-600 p-2 mr-10 rounded-xl text-lg font-semibold text-white items-center text-center justify-center cursor-pointer"
       >Search</button
     >
   </div>
